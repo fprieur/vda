85ed59f0d5067ea75d28ddd98812c482ad9be136
Fred Prieur <fprieur@gmail.com>
2016-08-02 10:39:36 -0400

    add units test

--- ports/tests/unit/testport.py
+++ ports/tests/unit/testport.py
@@ -5,7 +5,11 @@ from ports.ports import Ports
 class TestRetournePort(unittest.TestCase):
     def test(self):
         p = Ports()
-        self.assertEqual(p.ports(), "81 80 50001")
+        self.assertEqual(p.ports(), "50001 81 80")
+
+    def testNotEmptyPorts(self):
+        p = Ports()
+        self.assertIsNot(p.ports(), "", "la fonction port ne retourne pas rien")
 
 if __name__ == '__main__':
     unittest.main()
