2b32c7682b4a21f821a680fb89179ffb5dbd64ec
Fred Prieur <fprieur@gmail.com>
2016-08-02 09:56:27 -0400

    fix assert pour test sur travis

--- ports/tests/unit/testport.py
+++ ports/tests/unit/testport.py
@@ -5,7 +5,7 @@ from ports.ports import Ports
 class TestRetournePort(unittest.TestCase):
     def test(self):
         p = Ports()
-        self.assertEqual(p.ports(), "81 80 50001")
+        self.assertEqual(p.ports(), "80")
 
 if __name__ == '__main__':
     unittest.main()
